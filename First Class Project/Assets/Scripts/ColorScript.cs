aa3ea0b7bd61cc46abf094e4462982b73c2544aa
Laptop <xanives@my.uri.edu>
2020-02-04 16:25:44 -0500

    added last class's worth of examples to the first demo project

---
+++ First Class Project/Assets/Scripts/ColorScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour
+{
+    public Material Reddish_Pink;
+    public Material Green;
+    public AudioClip clip;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = Reddish_Pink;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            GetComponent<Renderer>().material = Green;
+            //audioSource.PlayUpdate
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<Renderer>().material = Reddish_Pink;
+        }
+    }
+}
